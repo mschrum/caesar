aec553f98a81e0a30c226c4527dc7ddd531f489e
Melissa Schrum <melissaschrum@gmail.com>
2016-08-22 19:58:42 -0500

    Caesar complete

--- main.py
+++ main.py
@@ -1,26 +1,70 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import cgi
 from caesar import encrypt
 
-class MainHandler(webapp2.RequestHandler):
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Caesar</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+        form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+        textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        h1 {
+                text-align: center;
+            }
+    </style>
+</head>
+<body>
+    <h1>Caesar</h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+text=""
+cypher_form = """
+<form action="/" method="post">
+    <label>
+    Rotate by:
+    </label>
+    <input type="text" name="rot" value="0"/>
+    <textarea type="text" name="text">{0}</textarea>
+    <br>
+    <input type="submit"/>
+    </form>
+"""
+
+class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        response = page_header + cypher_form.format("") + page_footer
+        self.response.write(response)
+
+    def post(self):
+        rotate = int(self.request.get("rot"))
+        text = self.request.get("text")
+        text_escaped = cgi.escape(text, quote=True)
+        text_crypt = encrypt(text_escaped, rotate)
+        response = page_header + cypher_form.format(text_crypt) + page_footer
+        self.response.write(response)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index)
 ], debug=True)
